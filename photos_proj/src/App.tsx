e41c03bf80f784ef64def79251a6807ea4b165e6
onionmccabbage <onionmccabbage@live.com>
2024-07-23 15:06:10 +0100

    sample solution for lab exercise 1

---
+++ photos_proj/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+// import Data from './components/data'
+// import CatComponent from './components/cat_component'
+import PhotoComponent from './components/photo_component'
+
+function App() {
+  return (
+    <>
+      <PhotoComponent />
+    </>
+  )
+}
+
+export default App
