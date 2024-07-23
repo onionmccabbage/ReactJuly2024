794d0421d36ba6f37ae06380eb52d38ca29fb1f4
onionmccabbage <onionmccabbage@live.com>
2024-07-23 11:32:28 +0100

    controlled component using a range

--- promise_proj/src/App.tsx
+++ promise_proj/src/App.tsx
@@ -7,14 +7,26 @@ import getCats from './services/catService'
 function App() {
   // we need a mechanism to retrieve API data
   const [cats, setCats] = useState([]) // start with an empty array
+  const [chooseWidth, setChooseWidth] = useState(700)
   // we cannot tell what may be retured, so our function is no longer pure
   // useEffect is for handling these side-effects
-  useEffect( ()=>{
+  useEffect( () => {
     // promises are 'thenable'
     // this catapi returns an evolving stream of data
-    setCats( getCats() )
-      // [] means only fetch when the component is first rendered
-  },[])
+    fetch('https://api.thecatapi.com/v1/images/search?limit=10')
+      .then((response) => { return response.json() })
+      .then((data) => {
+        // handle the returned data
+        setCats(data) // we return the retrieved JSON data
+      })
+    // setCats( async getCats())
+    // [] means only fetch when the component is first rendered
+  }, [])
+
+  // form field handler methods
+  const handleRangeChange = ()=>{
+    setChooseWidth(event?.target.value) // same as always
+  }
 
   return (
     <>
@@ -23,13 +35,20 @@ function App() {
       {/* this is a handy way to see the entire returned data */}
       {/* NB don't do this in production */}
       <pre>{JSON.stringify(cats)}</pre>
-      {cats.map( (cat:any)=>{
+      {/* we need a contolled form field to specify filter parameter */}
+    <form>
+      <label>
+      {chooseWidth}  
+      <input onChange={handleRangeChange} value={chooseWidth} type='range' min='500' step='100' max='2000' />
+      </label>
+    </form>
+      {cats.map((cat: any) => {
         return (
           <Fragment key={cat.id}>
             <Cat cat={cat} />
           </Fragment>
         )
-      } )}
+      })}
     </>
   )
 }
--- promise_proj/src/services/catService.tsx
+++ promise_proj/src/services/catService.tsx
@@ -1,9 +1,13 @@
-const getCats = ()=>{
-fetch('https://api.thecatapi.com/v1/images/search?limit=10')
-    .then((response) => { return response.json() })
-    .then((data) => {
-        // handle the returned data
-        return data // we return the retrieved JSON data
-    })
+import { useState } from "react"
+
+const getCats = async () => {
+    const [cats, setCats] = useState([])
+     await fetch('https://api.thecatapi.com/v1/images/search?limit=10')
+        .then((response) => { return response.json() })
+        .then((data) => {
+            // handle the returned data
+            setCats(data) // we return the retrieved JSON data
+            return cats
+        })
 } // end of getCats function
 export default getCats
