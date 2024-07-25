f1490743eda198382b9dbfd70bcc6590bee74609
onionmccabbage <onionmccabbage@live.com>
2024-07-25 16:26:31 +0100

    using styled components

---
+++ proj_style/src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import './App.css'
+
+// CSS 
+// tag styles apply automatically to every tag (override the default style)
+// class styles beat tag styles must be manually applied anywhere
+// id styles beat class styles must be manually applied ONCE
+
+// we need styled-components
+import styled from 'styled-components'
+//  we can declare the style for our custom component
+// even if they don't actually exist as tsx files
+// NB this might be better in a separate imported module
+const Container = styled.article`
+  border: 8px solid;
+  display:flex;
+  flex-direction:column;
+`
+const Header = styled.section`
+  padding: 10px 20px;
+  text-align: center;
+  color:#c0ffee;  
+`
+const H1 = styled.h1`
+  background-color: #369;
+`
+
+function App() {
+
+  return (
+    <>
+    {/* our Container will inherit the style of <article> */}
+      <Container>
+        <Header>Todo</Header>
+        <H1>Here is a heading</H1>
+      </Container>
+    </>
+  )
+}
+
+export default App
